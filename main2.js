3bcad80af01c2e6dfb93b84ce85c0227619b1582
Gustavo Hamburg <Gustavo.hamburg@outlook.com>
2020-08-19 12:30:29 -0300

    uso de requisições

--- main2.js
+++ main2.js
@@ -8,22 +8,22 @@ var nome = rep.owner.login;
 var img_p = document.createElement('img')
 img_p.setAttribute('src', rep.owner.avatar_url)
 
-var p_flw = rep.owner.following_url;
+var p_flw = "https://api.github.com/users/GHamb/following";
 
 var desc_rep = rep.description;
 
 var colab = rep.collaborators_url;
 
-var branches = rep.branches_url;
+var branches = "https://api.github.com/repos/GHamb/" + rep.name + "/branches";
 
-var list_commit = rep.commits_url;
+var list_commit = "https://api.github.com/repos/GHamb/" + rep.name + "/commits";
 
 var ling_util = rep.languages_url;
 
 
 document.getElementById('nome').innerHTML = nome;
 document.getElementById('img_p').appendChild(img_p)
-document.getElementById('desc').innerHTML = desc_rep;
+document.getElementById('desc').innerHTML = 'Descrição : ' + desc_rep;
 
 
 //requisição linguagem
@@ -33,7 +33,7 @@ req.open("GET", ling_util)
 req.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         for (i = 0; i < this.responseText.length; i++) {
-            document.getElementById('listlg').innerHTML = Object.keys(JSON.parse(this.responseText));
+            document.getElementById('listlg').innerHTML = 'Linguagens : <br>' + Object.keys(JSON.parse(this.responseText)) + '<img id="grf"src="img/grf.png" width="50px">';
 
         }
 
@@ -49,34 +49,17 @@ const req2 = new XMLHttpRequest();
 
 req2.open("GET", p_flw)
 req2.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById('peseg').innerHTML = JSON.parse(this.responseText)['message']
 
-    } else if (this.status == 404) {
-        document.getElementById('peseg').innerHTML = 'Not found'
+    if (this.readyState == 4 && this.status == 200) {
+        var salvajson = JSON.parse(this.responseText)
+        var salvar = document.getElementById('peseg')
+
+        salvar.innerHTML = 'Estou seguindo : '
+        salvar.innerHTML += (salvajson.length) + ' pessoas'
 
     }
 }
 req2.send()
-
-
-//requisição colaboração
-
-const req3 = new XMLHttpRequest();
-
-req3.open("GET", colab)
-req3.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById('colab').innerHTML = JSON.parse(this.responseText)['message']
-
-    } else if (this.status == 404) {
-        document.getElementById('colab').innerHTML = 'Not found'
-
-    }
-}
-req3.send()
-
-
 
 //requisição lista commits
 
@@ -84,11 +67,14 @@ const req4 = new XMLHttpRequest();
 
 req4.open("GET", list_commit)
 req4.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById('listcommit').innerHTML = JSON.parse(this.responseText)['message']
 
-    } else if (this.status == 404) {
-        document.getElementById('listcommit').innerHTML = 'Not found'
+    if (this.readyState == 4 && this.status == 200) {
+        var salvarc = JSON.parse(this.responseText)
+        for (var i = 0; i < salvarc.length; i++) {
+            document.getElementById('listcommit').innerText += ' ' + salvarc[i].commit.message + ','
+
+            console.log(salvarc[i].commit.message)
+        }
 
     }
 }
@@ -101,11 +87,10 @@ const req5 = new XMLHttpRequest();
 req5.open("GET", branches)
 req5.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-        document.getElementById('branches').innerHTML = JSON.parse(this.responseText)['message']
+        var salvarb = JSON.parse(this.responseText)
+        for (var i = 0; i < branches.length; i++)
 
-    } else if (this.status == 404) {
-        document.getElementById('branches').innerHTML = 'Not found'
-
+            document.getElementById('branches').innerText += ' ' + salvarb[i].name + ','
     }
 }
 req5.send()
